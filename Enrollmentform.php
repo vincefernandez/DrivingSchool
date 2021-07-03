a7705aca5f1d943bc6dfa9361fc466a5bae72ffd
Vincent Fernandez <vincentfernandez727@gmail.com>
2021-07-03 23:22:32 +0800

    first commit

--- Enrollmentform.php
+++ Enrollmentform.php
@@ -22,7 +22,7 @@ if(isset($_POST['EnrollSubmit'])){
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>enroll</title>
+    <title>Enrollment Form</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
